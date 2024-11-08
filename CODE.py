3c686d71ada8d563ad6a6a54359d486d5538990e
Taaftt <alejandro.andresvg@gmail.com>
2024-11-08 19:20:15 -0300

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -1,16 +1,15 @@
 import pandas as pd
-import gdown
+import requests
+from io import BytesIO
 import streamlit as st
 
-# URL directa del archivo en Google Drive
+# Enlace directo de descarga de Google Drive
 url = "https://drive.google.com/uc?export=download&id=1LlKcNqJA3zlbC76KvaYbIF4ZGPyEZoHy"
+response = requests.get(url)
 
-# Descargamos el archivo usando gdown
-output = 'archivo.csv'
-gdown.download(url, output, quiet=False)
+# Leer el archivo CSV desde la respuesta
+df = pd.read_csv(BytesIO(response.content))
 
-# Leemos el archivo CSV
-df = pd.read_csv(output)
+# Mostrar las primeras 5 filas en Streamlit
+st.write(df.head(5))
 
-# Mostramos las primeras 5 filas en Streamlit
-st.write(df.head())
