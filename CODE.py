4dda8299fdfb3cd5983ed48481af5b55510c018a
Taaftt <alejandro.andresvg@gmail.com>
2024-11-08 19:42:20 -0300

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -1,21 +1,21 @@
 import pandas as pd
 import gdown
 import streamlit as st
+
 def Cargar_CSV(id_url):
-  #Link del df del google drive
-  url = f'https://drive.google.com/uc?export=download&id={id_url}'
-  output = "archivo.csv"
-  
-  #Descargar nuestro drive con gdown
-  gdown.download(url, output, quiet=False)
+    # Link del archivo de Google Drive
+    url = f'https://drive.google.com/uc?export=download&id={id_url}'
+    output = "archivo.csv"
+    
+    # Descargar el archivo de Google Drive con gdown
+    gdown.download(url, output, quiet=False)
+    
+    # Leer el archivo CSV descargado
+    Lectura = pd.read_csv(output)
+    return Lectura
 
-  #Leer el drive
-  Lectura = pd.read_csv(output)
-  return lectura
+# Cargar el archivo CSV con el ID proporcionado
+Lectura = Cargar_CSV("1LlKcNqJA3zlbC76KvaYbIF4ZGPyEZoHy")
 
-df = Cargar_CSV("1LlKcNqJA3zlbC76KvaYbIF4ZGPyEZoHy")
-
-asd = df.head(10)
-# Mostrar las primeras 5 filas en Streamlit
-st.write(asd)
-
+# Mostrar las primeras 5 filas del DataFrame en Streamlit
+st.write(Lectura.head(5))
