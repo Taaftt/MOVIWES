479cde3e35c839c3fdd8fe931ef1683962d8247c
Taaftt <alejandro.andresvg@gmail.com>
2024-11-08 19:33:38 -0300

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -3,11 +3,11 @@ import gdown
 import streamlit as st
 def Cargar_CSV(id_url):
   #Link del df del google drive
-  url = f'https://drive.google.com/uc?export=download&id={+id_url}'
-  
+  url = f'https://drive.google.com/uc?export=download&id={id_url}'
+  output = "archivo.csv"
   #Descargar nuestro drive con gdown
   #Quiet = muestra msj del proceso de descargar pa ver si cargan bien
-  gdown.download(url, "archivo.csv", quiet=False)
+  gdown.download(url, output, quiet=False)
 
   #Leer el drive
   df = pd.read_csv(output)
