9fe75eb541926b9fbac7dfe2447013205010e3a5
Taaftt <alejandro.andresvg@gmail.com>
2024-11-08 19:14:21 -0300

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -3,7 +3,7 @@ import requests
 from io import BytesIO
 
 # Reemplaza la URL con el enlace de descarga directo de Google Drive
-url = "https://drive.google.com/file/d/1LlKcNqJA3zlbC76KvaYbIF4ZGPyEZoHy/view?usp=drive_link"
+url = "https://drive.google.com/file/d/1LlKcNqJA3zlbC76KvaYbIF4ZGPyEZoHy/view?usp=sharing"
 response = requests.get(url)
 df = pd.read_csv(BytesIO(response.content))
 ol= df.head(5)
