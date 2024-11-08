9837f918bf12505493f4a3f7383afaae7093c3e4
Taaftt <alejandro.andresvg@gmail.com>
2024-11-08 12:21:36 -0300

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -14,7 +14,7 @@ from io import StringIO
 # Función para descargar el archivo CSV desde Google Drive
 def csv_drive(file_id):
     # Construir la URL de descarga directa
-    url = f'https://drive.google.com/file/d/1LlKcNqJA3zlbC76KvaYbIF4ZGPyEZoHy/view?usp=drive_link{file_id}'
+    url = f'https://drive.google.com/uc?export=download&id={file_id}'
     # Realizar la solicitud HTTP
     response = requests.get(url)
     # Usamos StringIO para tratar el contenido como archivo en memoria
@@ -23,3 +23,13 @@ def csv_drive(file_id):
     df = pd.read_csv(csv_content, sep=',',encoding='ISO-8859-1',engine='python', on_bad_lines='skip').fillna(0)
     return df
   st.write(df)
+
+# Diccionario de ID de archivo de Google Drive y nombres de archivo
+file = {
+    "1LlKcNqJA3zlbC76KvaYbIF4ZGPyEZoHy": "movie.csv",
+}
+
+# Iterar sobre el diccionario de enlaces
+for file_id, file_name in file.items():
+    # Descargar y leer el CSV en un DataFrame
+    df = csv_drive(file_id)
